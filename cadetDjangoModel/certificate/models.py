d03dac4ba4d83d2287dd7d140cc335d0a97c5125
guizindev <guilhermevasco2015@gmail.com>
2025-01-10 15:19:13 -0300

    migrations

--- cadetDjangoModel/certificate/models.py
+++ cadetDjangoModel/certificate/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 from cadet.models import Cadet
 
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
 class Certificate(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(default="teste")
-    student = models.ForeignKey(Cadet, on_delete=models.CASCADE)
+    skills_required = models.ManyToManyField(Skill, related_name="certificates")
     issued_at = models.DateTimeField(auto_now_add=True)
 
+class UserSkill(models.Model):
+    user = models.ForeignKey(
+        Cadet,
+        on_delete=models.CASCADE,
+        related_name="user_skills"
+    )
+    skill = models.ForeignKey(
+        Skill,
+        on_delete=models.CASCADE,
+        related_name="user_skills"
+    )
