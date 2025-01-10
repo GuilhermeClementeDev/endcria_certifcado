9d824d0f6346b1cffccd7d4903577644dd39a757
guizindev <guilhermevasco2015@gmail.com>
2025-01-10 15:06:10 -0300

    some changes

--- cadetDjangoModel/certificate/models.py
+++ cadetDjangoModel/certificate/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from cadet.models import Cadet
 
-# Create your models here.
+class Certificate(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(default="teste")
+    student = models.ForeignKey(Cadet, on_delete=models.CASCADE)
+    issued_at = models.DateTimeField(auto_now_add=True)
+
--- cadetDjangoModel/certificate/urls.py
+++ cadetDjangoModel/certificate/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
 # Class APIView para melhor personalização:
-from .views import
 
 urlpatterns = [
-    path('cadets/', .as_view(), name='cadet-list'),
-	path('staff/', .as_view(), name='cadet-list')
+
 ]
