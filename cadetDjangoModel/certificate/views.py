e336a1c3123ebf4cb37754337d4e483b24faf586
guizindev <guilhermevasco2015@gmail.com>
2025-01-12 20:04:06 -0300

    roles

--- cadetDjangoModel/certificate/views.py
+++ cadetDjangoModel/certificate/views.py
@@ -2,13 +2,11 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 # from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
 from django.http import HttpResponse
-
 from rest_framework import status
-
 from .models import Certificate, Skill
 from .serializers import CertificateSerializer, SkillSerializer
+from .permissions import HasRolePermission
 
 class  CertificateListCreateAPIView(APIView):
     #Autenticação com Roles
